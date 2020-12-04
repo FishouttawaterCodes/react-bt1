5839c93f003b6672232c7f5ba9adc837f5d8971e
Oluwaseun Olu-Young <oluwaseunoluyoung@gmail.com>
2020-12-04 04:22:55 +0100

    first react project

---
+++ src/NoMatch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+export const NoMatch = () => (
+  <Container>
+    <div>
+      <h2>404 page not found</h2>
+    </div>
+  </Container>
+)
+
